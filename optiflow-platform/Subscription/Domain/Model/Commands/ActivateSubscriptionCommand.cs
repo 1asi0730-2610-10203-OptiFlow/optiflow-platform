df4e2d343938b13b779c7e495d1fff88ac531362
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-08 19:45:08 -0500

    feat(subscription): add ActivateSubscriptionCommand

---
+++ optiflow-platform/Subscription/Domain/Model/Commands/ActivateSubscriptionCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace optiflow_platform.Subscription.Domain.Model.Commands;
+
+public record ActivateSubscriptionCommand(int SubscriptionId, string Tier, DateTimeOffset StartDate, DateTimeOffset EndDate);
