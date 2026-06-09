bb7227902f8ee6f0e729c7b4275056da352d8810
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-08 19:47:29 -0500

    feat(subscription): add CancelSubscriptionCommand

--- optiflow-platform/Subscription/Domain/Model/Commands/ActivateSubscriptionCommand.cs
+++ optiflow-platform/Subscription/Domain/Model/Commands/ActivateSubscriptionCommand.cs
@@ -1,3 +1,4 @@
 ﻿namespace optiflow_platform.Subscription.Domain.Model.Commands;
 
+
 public record ActivateSubscriptionCommand(int SubscriptionId, string Tier, DateTimeOffset StartDate, DateTimeOffset EndDate);
---
+++ optiflow-platform/Subscription/Domain/Model/Commands/CancelSubscriptionCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace optiflow_platform.Subscription.Domain.Model.Commands;
+
+
+public record CancelSubscriptionCommand(int SubscriptionId);
