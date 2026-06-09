c7eac5028fb672cdec176d8da906b7845ec25bd9
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-08 20:09:51 -0500

    feat(subscription): add queries

---
+++ optiflow-platform/Subscription/Domain/Model/Queries/GetPaymentByIdQuery.cs
@@ -0,0 +1,3 @@
+namespace optiflow_platform.Subscription.Domain.Model.Queries;
+
+public record GetPaymentByIdQuery(int Id);
---
+++ optiflow-platform/Subscription/Domain/Model/Queries/GetSubscriptionByIdQuery.cs
@@ -0,0 +1,3 @@
+namespace optiflow_platform.Subscription.Domain.Model.Queries;
+
+public record GetSubscriptionByIdQuery(int Id);
