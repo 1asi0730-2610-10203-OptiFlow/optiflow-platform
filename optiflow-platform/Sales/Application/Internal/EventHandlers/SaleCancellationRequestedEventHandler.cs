488177648252fea204b8c52e05a1436b40898032
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-14 13:30:45 -0500

    feat(sales): add SaleCancellationRequestEventHandler

---
+++ optiflow-platform/Sales/Application/Internal/EventHandlers/SaleCancellationRequestedEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using optiflow_platform.Sales.Application.Services;
+using optiflow_platform.Sales.Domain.Model.Commands;
+using optiflow_platform.Sales.Domain.Model.Events;
+using optiflow_platform.Shared.Application.Internal.EventHandlers;
+using optiflow_platform.Sales.Interfaces.Acl;
+
+namespace optiflow_platform.Sales.Application.Internal.EventHandlers;
+
+public class SaleCancellationRequestedEventHandler(ILabAndOrdersContextFacade labAndOrdersContextFacade,
+ISaleCommandService saleCommandService) : IEventHandler<SaleCancellationRequestedEvent>
+{
+
+    public Task Handle(SaleCancellationRequestedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        return On(domainEvent, cancellationToken);
+    }
+
+
+    private async Task On(SaleCancellationRequestedEvent domainEvent, CancellationToken cancellationToken) {
+        var status = await labAndOrdersContextFacade.FetchWorkOrderStatusBySaleId(domainEvent.SaleId, cancellationToken);
+
+        if (status == "IN_PRODUCTION")
+        {
+            Console.WriteLine("Cannot cancel sale {0}: lab order is in production", domainEvent.SaleId);
+            return;
+        }
+        await saleCommandService.Handle(new CancelSaleCommand(domainEvent.SaleId, status ?? "IN_PRODUCTION"),  cancellationToken);
+        
+    }
+}
