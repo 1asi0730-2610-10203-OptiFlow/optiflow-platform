4165ace67c50483193c0808063389c1f99079525
Llummo <167897805+Llummo@users.noreply.github.com>
2026-06-15 00:17:13 -0500

    feat(subscription): delete check plan end date command

--- optiflow-platform/Subscription/Domain/Model/Commands/CheckSubscriptionEndDateCommand.cs
+++
@@ -1,9 +0,0 @@
-using optiflow_platform.Subscription.Domain.Model.ValueObjects;
-
-namespace optiflow_platform.Subscription.Domain.Model.Commands;
-
-/// <summary>
-///     Issued by the system to check whether the subscription end date has been reached.
-/// </summary>
-/// <param name="SubscriptionId"></param>
-public record CheckSubscriptionEndDateCommand(SubscriptionId SubscriptionId);
