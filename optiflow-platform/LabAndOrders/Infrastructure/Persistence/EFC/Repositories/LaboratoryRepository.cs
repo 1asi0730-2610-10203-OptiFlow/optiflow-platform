33ddf1f59ee8bec94dccde08abd5b98cbe1d72c2
luuu6 <lucianamechan@gmail.com>
2026-06-15 00:29:31 -0500

    feat(LabAndOrders): add LaboratoryRepository

--- optiflow-platform/LabAndOrders/Infrastructure/Persistence/EFC/Repositories/LaboratoryRepository.cs
+++ optiflow-platform/LabAndOrders/Infrastructure/Persistence/EFC/Repositories/LaboratoryRepository.cs
@@ -1,4 +1,4 @@
-using optiflow_platform.LabAndOrders.Domain.Model.Entities;
+using optiflow_platform.LabAndOrders.Domain.Model.Aggregates;
 using optiflow_platform.LabAndOrders.Domain.Repositories;
 using optiflow_platform.Shared.Infrastructure.Persistence.EFC.Configuration;
 using optiflow_platform.Shared.Infrastructure.Persistence.EFC.Repositories;
