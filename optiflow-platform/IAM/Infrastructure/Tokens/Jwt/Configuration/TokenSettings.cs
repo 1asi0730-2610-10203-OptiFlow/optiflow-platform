55a63c845b9e685a0e6361cb1dfa9be8e20f2a9b
Patto04 <morochomariana827@gmail.com>
2026-06-19 09:39:43 -0500

    feat(iam): implement token generation service and configuration.

---
+++ optiflow-platform/IAM/Infrastructure/Tokens/Jwt/Configuration/TokenSettings.cs
@@ -0,0 +1,7 @@
+namespace atelier_platform_aplicaciones_web.IAM.Infrastructure.Tokens.Jwt.Configuration;
+
+public class TokenSettings
+{
+    public required string Secret { get; set; }
+    public int ExpirationDays { get; set; }
+}
