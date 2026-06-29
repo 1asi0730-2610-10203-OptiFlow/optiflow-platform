11b65db276d67be539db4cc26b2ad13934b17f3f
Patto04 <morochomariana827@gmail.com>
2026-06-29 18:32:29 -0500

    feat(iam): update user query service interface.

--- optiflow-platform/IAM/Application/Services/IUserQueryService.cs
+++ optiflow-platform/IAM/Application/Services/IUserQueryService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using optiflow_platform.IAM.Domain.Model.Aggregates;
@@ -9,4 +10,5 @@ public interface IUserQueryService
 {
     Task<User?> Handle(GetUserByIdQuery query, CancellationToken cancellationToken);
     Task<User?> Handle(GetUserByEmailQuery query, CancellationToken cancellationToken);
+    Task<IEnumerable<User>> Handle(GetAllUsersQuery query, CancellationToken cancellationToken);
 }
