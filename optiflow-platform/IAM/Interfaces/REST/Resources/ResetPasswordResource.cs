08709157c205e6358262c523a382fb7511986c08
Llummo <167897805+Llummo@users.noreply.github.com>
2026-07-07 15:26:48 -0500

    feat(iam): validate password reset, password change, email update and recovery payloads

--- optiflow-platform/IAM/Interfaces/REST/Resources/ResetPasswordResource.cs
+++ optiflow-platform/IAM/Interfaces/REST/Resources/ResetPasswordResource.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace optiflow_platform.IAM.Interfaces.REST.Resources;
 
-public record ResetPasswordResource(string Token, string NewPassword);
+public record ResetPasswordResource(
+    [Required(ErrorMessage = "iam.error.token.required")] string Token,
+    [Required(ErrorMessage = "iam.error.password.required")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "iam.error.password.tooShort")] string NewPassword);
