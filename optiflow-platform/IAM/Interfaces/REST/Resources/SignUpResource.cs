9690ed5f10e762f1ad9775ebb8110e963067d81e
Llummo <167897805+Llummo@users.noreply.github.com>
2026-07-07 13:25:52 -0500

    feat(iam): validate email format and enforce minimum password length

--- optiflow-platform/IAM/Interfaces/REST/Resources/SignUpResource.cs
+++ optiflow-platform/IAM/Interfaces/REST/Resources/SignUpResource.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations;
 namespace optiflow_platform.IAM.Interfaces.REST.Resources;
 
 public record SignUpResource(
-    [Required(ErrorMessage = "iam.error.email.required")] string Email,
-    [Required(ErrorMessage = "iam.error.password.required")] string Password,
+    [Required(ErrorMessage = "iam.error.email.required")]
+    [EmailAddress(ErrorMessage = "iam.error.email.invalid")]
+    [StringLength(100, ErrorMessage = "iam.error.email.tooLong")] string Email,
+    [Required(ErrorMessage = "iam.error.password.required")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "iam.error.password.tooShort")] string Password,
     string? UserType = null);
