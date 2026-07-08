0e08f2fda4210d3c082118b9a46022a0692a28a0
Miavcl <miavalentinacl@gmail.com>
2026-07-08 13:25:07 -0500

    fix: notifications

--- optiflow-platform/PatientCenter/Application/Internal/QueryServices/PatientNotificationQueryService.cs
+++ optiflow-platform/PatientCenter/Application/Internal/QueryServices/PatientNotificationQueryService.cs
@@ -8,7 +8,39 @@ namespace optiflow_platform.PatientCenter.Application.Internal.QueryServices;
 public class PatientNotificationQueryService(IPatientNotificationRepository notificationRepository)
     : IPatientNotificationQueryService
 {
-    public async Task<IEnumerable<PatientNotification>> Handle(GetNotificationsByPatientIdQuery query,
-        CancellationToken cancellationToken = default) =>
-        await notificationRepository.FindByPatientIdAsync(query.PatientId, cancellationToken);
+
+    public async Task<IEnumerable<PatientNotification>> Handle(GetNotificationsByPatientIdQuery query, CancellationToken cancellationToken)
+    {
+        var patientId = query.PatientId;
+
+        var existingNotifications = await _notificationRepository.GetByPatientIdAsync(patientId, cancellationToken);
+
+        var currentOrders = await _patientOrderQueryService.Handle(new GetOrdersByPatientIdQuery(patientId), cancellationToken);
+
+        bool checkNewNotifications = false;
+
+        foreach (var order in currentOrders)
+        {
+            var alreadyNotified = existingNotifications.Any(n => n.WorkOrderId == order.Id && n.Message.Contains(order.Status.ToString()));
+
+            if (!alreadyNotified)
+            {
+                var createCommand = new CreateNotificationCommand(
+                    patientId,
+                    order.Id,
+                    $"Tu pedido N° {order.OrderNumber} ha cambiado al estado: {order.Status}",
+                    "PENDING"
+                );
+                
+                await _notificationCommandService.Handle(createCommand, cancellationToken);
+                checkNewNotifications = true;
+            }
+        }
+        if (checkNewNotifications)
+        {
+            existingNotifications = await _notificationRepository.GetByPatientIdAsync(patientId, cancellationToken);
+        }
+
+        return existingNotifications;
+    }
 }
